ca75caa899940aad6782a76d88bf120a11d3488a
EmelKesten <kestenemel@gmail.com>
2022-06-16 00:16:57 +0200

    bootstrap

--- search/search.js
+++ search/search.js
@@ -1,20 +1,31 @@
 const txtInput = document.getElementById('txt-input'); 
+const content = document.querySelector('.content');
+const mostPopular = document.querySelector('#mostPopular');
+const nowPlaying = document.querySelector('#nowPlaying');
+const upComing = document.querySelector('#upComing');
 
 txtInput.addEventListener("keypress", (event) => {
-    if (txtInput.value) {
-        event.preventDefault();
+    if (txtInput.value.length >= 3) {
         search(txtInput);
     }
+    else if(txtInput.value.length === 0) {
+        mostPopular.classList.remove('noShow');
+        nowPlaying.classList.remove('noShow');
+        upComing.classList.remove('noShow');
+    }
+     
 });
 
 async function search(input) {
     const txtInputValue = input.value;
+    console.log(txtInputValue);
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${txtInputValue}&page=1&include_adult=true`);
     const data = await response.json();
-    console.log(data);
+    mostPopular.classList.add('noShow');
+    nowPlaying.classList.add('noShow');
+    upComing.classList.add('noShow');
     if (data){
         const results = data.results;
-        divMostPopular.innerHTML = '';
         results.forEach(article => {
             const articleDiv = document.createElement('div');
             articleDiv.classList.add('article');
@@ -22,7 +33,7 @@ async function search(input) {
                 <img src="https://image.tmdb.org/t/p/w500/${article.poster_path}" alt="Not able to get poster">
                 <h4>${article.title}</h4>
             `;
-            divMostPopular.appendChild(articleDiv);
+            content.appendChild(articleDiv);
         }); 
     }
 }
--- upComing/upComing.js
+++ upComing/upComing.js
@@ -1,7 +1,7 @@
-const nowPlayingDiv = document.getElementById('nowPlaying-inner');
+const upComingDiv = document.getElementById('upComing-inner');
 
-async function getNowPlaying() {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=1`);
+async function getUpComing() {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`);
     const data = await response.json();
     if (data){
         const results = data.results;
@@ -12,14 +12,9 @@ async function getNowPlaying() {
                 <img src="https://image.tmdb.org/t/p/w500/${article.poster_path}" alt="${article.title}">
                 <h4>${article.title}</h4>
             `;
-            nowPlayingDiv.appendChild(articleDiv);
+            upComingDiv.appendChild(articleDiv);
         });
     }
 }
 
-getNowPlaying();
-
-
-
-
-
+getUpComing();
