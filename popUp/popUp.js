3f4597d83a2eac29728d9835b49c47c857ecf59c
EmelKesten <kestenemel@gmail.com>
2022-06-18 15:36:02 +0200

    popUp shit

--- popUp/popUp.js
+++ popUp/popUp.js
@@ -3,6 +3,8 @@ const popUpDiv = document.querySelector('#popUp');
 async function getPopUp(id) {
     const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=en-US`);
     const data = await response.json();
+    const vidResponse = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${api_key}&language=en-US`);
+    const vidData = await vidResponse.json();
     console.log(id, 'id');
     if (data){
         const results = data;
@@ -13,12 +15,17 @@ async function getPopUp(id) {
                 background-position: center;
                 background-repeat: no-repeat;
             ">
+                    <button id="close">X</button>
                     <h4>${results.title}</h4>
                     <p>${results.overview}</p>
-                    <button id="close">X</button>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/${vidData.results[0].key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         `;
         document.querySelector('#close').addEventListener('click', () => {
+            popUpDiv.innerHTML = '';
+            popUpDiv.classList.add('noShow')
+        })
+        popUpDiv.addEventListener('click', () => {
             popUpDiv.innerHTML = '';
             popUpDiv.classList.add('noShow')
         })
